5bb0688b407c0eef41518488c1318484e9e2cecf
Shayne <IharKuchinsky@users.noreply.github.com>
2017-10-03 13:42:31 +0300

    Edit engine capacity presentation format

--- Creational/AbstractFactory/Helpers/InfoPrinter.cs
+++ Creational/AbstractFactory/Helpers/InfoPrinter.cs
@@ -9,7 +9,7 @@ namespace AbstractFactory.Helpers
         {
             Console.WriteLine($"Model: {car.Info.Model}, {car.Info.Year}");
             Console.WriteLine($"Body: {car.Body.Type}, {car.Body.Color}");
-            Console.WriteLine($"Engine: {car.Engine.Capacity / 100} ltr., {car.Engine.Power} HP");
+            Console.WriteLine($"Engine: {(double)car.Engine.Capacity / 1000} ltr., {car.Engine.Power} HP");
 
             Console.WriteLine();
         }
