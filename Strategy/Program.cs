671843c3c95070e79d49357e4284fd685b7574c1
Shayne <IharKuchinsky@users.noreply.github.com>
2017-12-11 13:41:51 +0300

    Added Strategy sample

---
+++ Strategy/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Strategy.Entities;
+
+namespace Strategy
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Car audi = new Car("Audi", "A7", EngineType.Diesel);
+			Car bmw = new Car("BMW", "X6", EngineType.Gasoline);
+			Car tesla = new Car("Tesla", "P90", EngineType.Electric);
+
+			audi.PrintInfo();
+			bmw.PrintInfo();
+			tesla.PrintInfo();
+
+			Console.Read();
+		}
+	}
+}
