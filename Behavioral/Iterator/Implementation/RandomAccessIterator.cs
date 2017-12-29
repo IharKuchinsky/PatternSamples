b4339ee23773df8bc2fc6f60989aa1a3176e52d2
Shayne <IharKuchinsky@users.noreply.github.com>
2017-12-29 13:36:23 +0300

    Added Iterator sample

---
+++ Behavioral/Iterator/Implementation/RandomAccessIterator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Iterator.Implementation
+{
+	class RandomAccessIterator<T> : IEnumerator<T>
+	{
+		private readonly Random _random = new Random(DateTime.Now.Millisecond);
+		private int[] _visitedIndexes;
+		private int _visitedElementsCount = 0;
+		private int _currentIndex = 0;
+		private readonly int _count;
+		private readonly RandomAccesList<T> _randomAccesList;
+
+		public RandomAccessIterator(RandomAccesList<T> list)
+		{
+			_randomAccesList = list;
+			_count = list.Count;
+			_currentIndex = NextRandom();
+			_visitedIndexes = ResetRandomIterator();
+		}
+
+		public bool MoveNext()
+		{
+			if (_visitedElementsCount < _count) GetNextRandomIndex();
+			else return false;
+			
+			return _visitedElementsCount <= _count;
+		}
+
+		public void Reset()
+		{
+			_visitedIndexes = ResetRandomIterator();
+			_currentIndex = NextRandom();
+		}
+
+		public T Current
+		{
+			get
+			{
+				if (_currentIndex < 0 || _currentIndex > _count) throw new InvalidEnumArgumentException();
+				return _randomAccesList[_currentIndex];
+			}
+		}
+
+		object IEnumerator.Current => Current;
+
+		public void Dispose()
+		{
+			//throw new NotImplementedException();
+		}
+
+		private int NextRandom()
+		{
+			return _random.Next(0, _count);
+		}
+
+		private int[] ResetRandomIterator()
+		{
+			_visitedElementsCount = 0;
+			return Enumerable.Repeat(-1, _count).ToArray();
+		}
+
+		private void GetNextRandomIndex()
+		{
+			while (Array.Exists(_visitedIndexes, element => element == _currentIndex)) _currentIndex = NextRandom();
+
+			_visitedIndexes[_visitedElementsCount] = _currentIndex;
+			_visitedElementsCount++;
+		}
+	}
+}
---
+++ Behavioral/Iterator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using Iterator.Implementation;
+
+namespace Iterator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			RandomAccesList<int> randomAccesListGeneric = new RandomAccesList<int>{1, 2, 3, 4, 5};
+			randomAccesListGeneric.Add(6);
+
+			IEnumerable randomAccesList = randomAccesListGeneric;
+
+			foreach (var item in randomAccesListGeneric) Console.WriteLine(item);
+			Console.WriteLine();
+			foreach (var item in randomAccesList) Console.WriteLine(item);
+
+			Console.Read();
+		}
+	}
+}
